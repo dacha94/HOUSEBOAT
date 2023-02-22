afa71631c1f14ac578c68c0067569ecb06f4bc1a
Дарья Слеповичева <85016619+dacha94@users.noreply.github.com>
2023-02-22 12:51:10 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,24 +43,9 @@ let swiper = new Swiper(".mySwiper", {
   }
 });  
 
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-const menuLinks = document.querySelectorAll('.menu__link');
+const header = document.querySelector('.header');
+const burger = document.querySelector('.header__burger');
 
-if(menuLinks) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener('click', () => {
-            document.body.classList.remove('_lock');
-            iconMenu.classList.remove('_active');
-            menuBody.classList.remove('_active');
-        })
-    })
-}
-
-if (iconMenu) {
-    iconMenu.addEventListener('click', e => {
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    })
-}
+burger.addEventListener('click', () => {
+  header.classList.toggle('open');
+})
